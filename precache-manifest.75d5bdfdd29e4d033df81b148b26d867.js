93b14b86f08b6b316ff1800da3d7756939e10b68
sebasvega95 <sebastian@landing.ai>
2020-05-09 18:29:05 -0500

    Updates

---
+++ precache-manifest.75d5bdfdd29e4d033df81b148b26d867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30863c74057329622d7fbc42ac99df0",
+    "url": "/remove-squares-game/index.html"
+  },
+  {
+    "revision": "1b42920b22d05496b9f5",
+    "url": "/remove-squares-game/static/css/main.9899037b.chunk.css"
+  },
+  {
+    "revision": "0905b612cc46fd889c9d",
+    "url": "/remove-squares-game/static/js/2.a933a56b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/remove-squares-game/static/js/2.a933a56b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b42920b22d05496b9f5",
+    "url": "/remove-squares-game/static/js/main.2141a508.chunk.js"
+  },
+  {
+    "revision": "9bce2e2f88660fcf8ac1",
+    "url": "/remove-squares-game/static/js/runtime-main.b7aa8ed6.js"
+  }
+]);
